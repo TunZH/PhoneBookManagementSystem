33358408aed1ce6e44687f63fe201d8394e74b55
IndianMaster <khangnhd8590@ut.edu.vn>
2026-05-03 23:18:17 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -53,3 +53,67 @@ public:
     }
 };
 // thanh vien 2
+class PhoneBook {
+private:
+    vector<Contact> list;
+    bool isPhoneExists(const string& checkPhone) {
+        for (const auto& c : list) {
+            if (c.phone == checkPhone) return true;
+        }
+        return false;
+    }
+
+public:
+    void load() {
+        ifstream file("ContactList.txt");
+        if (!file) return;
+        Contact c;
+        while (getline(file, c.firstName)) {
+            getline(file, c.lastName);
+            file >> c.gender;
+            file.ignore();
+            getline(file, c.phone);
+            getline(file, c.city);
+            list.push_back(c);
+        }
+    }
+
+    void save() {
+        ofstream file("ContactList.txt");
+        for (auto &c : list) {
+            file << c.firstName << "\n" << c.lastName << "\n"
+                 << c.gender << "\n" << c.phone << "\n" << c.city << "\n";
+        }
+    }
+
+    void addContact() {
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        string p;
+        cout << "Enter phone number (Primary Key): ";
+        getline(cin, p);
+        if (p.empty() || isPhoneExists(p)) {
+            cout << "[Error] Invalid or duplicate phone number!\n";
+            return;
+        }
+        Contact c;
+        c.inputDetails(p);
+        list.push_back(c);
+        cout << "[Success] Contact added to memory.\n";
+    }
+
+    
+    void updateContact() {
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        string key;
+        cout << "Enter phone number to update: ";
+        getline(cin, key);
+        for (auto &c : list) {
+            if (c.phone == key) {
+                c.inputDetails(key);
+                cout << "[Success] Contact updated.\n";
+                return;
+            }
+        }
+        cout << "[Error] Not found.\n";
+    }
+//thanh vien 3
