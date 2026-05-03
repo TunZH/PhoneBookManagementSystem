7414f7e10d75af9bff6f37480a744c9557c8e32f
giahuy4556a-web <giahuy4556a@gmail.com>
2026-05-03 23:21:01 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -155,3 +155,52 @@ void deleteContact() {
         for (auto &c : list) c.display(i++);
         cout << "------------------------\n";
     }
+// thanh vien 4
+void searchByName() {
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        string key;
+        cout << "Enter name to search: ";
+        getline(cin, key);
+        key = toLower(key);
+        int found = 0;
+        for (auto &c : list) {
+            if (toLower(c.firstName) == key || toLower(c.lastName) == key) {
+                if (found == 0) cout << "\nFound Contact(s):\n";
+                c.display(++found);
+            }
+        }
+        if (!found) cout << "Not found.\n";
+    }
+
+    void searchByPhone() {
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        string key;
+        cout << "Enter phone to search: ";
+        getline(cin, key);
+        int found = 0;
+        for (auto &c : list) {
+            if (c.phone == key) {
+                if (found == 0) cout << "\nFound Contact(s):\n";
+                c.display(++found);
+            }
+        }
+        if (!found) cout << "Not found.\n";
+    }
+
+    
+    void searchByCity() {
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        string key;
+        cout << "Enter city to filter: ";
+        getline(cin, key);
+        key = toLower(key);
+        int found = 0;
+        for (auto &c : list) {
+            if (toLower(c.city) == key) {
+                if (found == 0) cout << "\nFound Contact(s):\n";
+                c.display(++found);
+            }
+        }
+        if (!found) cout << "Not found.\n";
+    }
+};
